1c67ab3a7c9821102b33c2f52fe3e8652872cb52
Wendy Phillips <wendysphillips@users.noreply.github.com>
2017-02-18 17:33:57 -0800

    Final version

--- plot2.R
+++ plot2.R
@@ -5,7 +5,7 @@ subset <- filter(energy.data, Date >= "2007-02-01" & Date < "2007-02-03")
 subset$newdate <- with(subset, as.POSIXct(paste(Date, Time),format="%Y-%m-%d %H:%M:%S"))
 subset$datetime <- strptime(subset$newdate, "%Y-%m-%d %H:%M:%S")
 
-png(filename ="plot3.png", width = 480,  height = 480, units ="px")
+png(filename ="plot2.png", width = 480,  height = 480, units ="px")
 plot(subset$datetime, subset$Global_active_power, type="l", ylab="Global active power (kilowatts)", xlab= "Day")
 axis(side =1, at = c(1, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
 dev.off()
